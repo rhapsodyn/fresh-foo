e0359b6aeefcff5c45e140a65215d80bdf2378bb
tassadarliu <rhapsodyn@gmail.com>
2022-06-24 16:18:40 +0800

    chore: second try

--- routes/api/timeout.ts
+++ routes/api/timeout.ts
@@ -1,14 +1,23 @@
 import { HandlerContext } from "$fresh/server.ts";
+import { crypto } from "crypto";
 
 const BODY_TEXT = "Up to 10ms CPU time per request";
 
-export const handler = (
-  _req: Request,
-  _ctx: HandlerContext
-): Promise<Response> => {
+/**
+ * try 1
+ */
+const handler_1 = (_req: Request, _ctx: HandlerContext): Promise<Response> => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(new Response(BODY_TEXT));
     }, 200);
   });
+};
+
+export const handler = (_req: Request, _ctx: HandlerContext): Response => {
+  let text = BODY_TEXT;
+  for (let i = 0; i < 1000; i++) {
+    text += "\n" + crypto.randomUUID();
+  }
+  return new Response(text);
 };
